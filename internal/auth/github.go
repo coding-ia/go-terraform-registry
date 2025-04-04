954f75bca139300b4967e29a88fe5b3e893cbc9a
bjunker <brian.junker@gmail.com>
2025-04-04 09:36:23 -0700

    Update github.go

--- internal/auth/github.go
+++ internal/auth/github.go
@@ -2,14 +2,15 @@ package auth
 
 import (
 	"context"
-	"fmt"
 	"github.com/google/go-github/v69/github"
+	"log"
 )
 
 func GetGitHubUserName(ctx context.Context, client *github.Client, token string) (*string, error) {
 	user, _, err := client.Users.Get(ctx, "")
 	if err != nil {
-		return nil, fmt.Errorf("error getting user: %s", err)
+		log.Printf("Error getting user: %v", err)
+		return nil, err
 	}
 
 	return user.Login, nil
